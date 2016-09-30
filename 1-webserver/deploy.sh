8be50f62e56b4815f5d65aa7c6ad28d08bc6d2de
Tim Dysinger <tim@dysinger.net>
2016-09-30 15:41:49 -0700

    updated org-babel-tangle output

--- 1-webserver/deploy.sh
+++ 1-webserver/deploy.sh
@@ -1,5 +1,10 @@
 #!/bin/bash -eux
-kubectl create -f $PWD
+kubectl create -f .
+sleep 10
+kubectl get service webserver
+kubectl get deployment webserver
+kubectl get pods webserver
+
 kubectl describe service webserver
 kubectl describe deployment webserver
 kubectl describe pods webserver
--- 1-webserver/update.sh
+++ 1-webserver/update.sh
@@ -1,11 +1,15 @@
 #!/bin/bash -eux
 eval $(minikube docker-env)
-docker build -t webserver:0.1.0 $PWD
+docker build -t webserver:0.1.0 .
 
 kubectl set image deployment/webserver webserver=webserver:0.1.0
 sleep 10
-kubectl describe service webserver
-kubectl describe deployment webserver
-kubectl describe pods webserver
+kubectl get service webserver
+kubectl get deployment webserver
+kubectl get pods webserver
+
+minikube dashboard
 
 minikube service webserver
+
+curl -sSL $(minikube service --url webserver)
